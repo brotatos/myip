abf9ac4f8c50f71269f186605ba4e327f1e8933a
Robin Choudhury <xrobinc@gmail.com>
2014-08-09 20:16:17 -0700

    Setup js and css.
    The js sets the background based off the first 3
    parts of the ip. The css gives the font a white
    and black bordered look.

---
+++ myip/static/js/myip.js
@@ -0,0 +1,21 @@
+/*
+ * Oh hi there.
+ *
+ * I just wanted to dabble in some very basic jQuery.
+ */
+
+$(document).ready(function() {
+    var $body = $(document.body);
+    var $ip = $("#ip");
+    var ip_height = $ip.height() / 2;
+    var window_height = $ip.height() / 2;
+    var middle = window_height - ip_height;
+    var colors = $ip.text().split('.');
+    var red = colors[0];
+    var blue = colors[1];
+    var green = colors[2];
+
+    $ip.css("margin-top", middle + "px");
+    $body.css("background-color",
+              "rgb(" + red + "," + blue + "," + green + ")");
+});
