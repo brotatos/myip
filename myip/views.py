fb7766a4155a229f31af0e33d1b2aedc1d2ff380
Robin Choudhury <xrobinc@gmail.com>
2014-08-09 20:16:01 -0700

    Remove json route for retrieving the ip.

--- myip/views.py
+++ myip/views.py
@@ -1,14 +1,9 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, render_template
 
 
 bp = Blueprint('views', __name__, url_prefix='')
 
 
-@bp.route('/ip', methods=['GET'])
-def get_ip():
-    return jsonify(ip=request.remote_addr)
-
-
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', ip=request.remote_addr)
