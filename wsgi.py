f334c9770dd7d9fb38fd5c118df806dbf532d17a
Robin Choudhury <xrobinc@gmail.com>
2014-08-09 20:15:38 -0700

    Add application variable for the procfile.

--- wsgi.py
+++ wsgi.py
@@ -11,7 +11,9 @@ from werkzeug.wsgi import DispatcherMiddleware
 
 from myip.factory import create_app
 
+application = create_app()
+
 
 if __name__ == "__main__":
-    run_simple('localhost', 5000, create_app(), use_reloader=True,
+    run_simple('localhost', 5000, application, use_reloader=True,
                use_debugger=True, use_evalex=True)
